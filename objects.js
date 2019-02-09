b984ed82e7856d35cfe05f4c1444780ef55c2261
Aditya Waruwu <aditya.william96@gmail.com>
2019-02-09 05:01:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Anji : 'Dia'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = 'songTitle';
+  playlist[artistName] = 'songTitle';
   return playlist;
 }
